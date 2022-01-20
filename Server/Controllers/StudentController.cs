8ef50287ea2f4cbb1fe567523b92c45471805b82
DaoHanh32 <77731242+DaoHanh32@users.noreply.github.com>
2022-01-20 11:53:14 +0700

    them duoc custom validate

---
+++ Server/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DemoValidatorCustom.Shared.Models;
+
+namespace DemoValidatorCustom.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        readonly List<string> useNameList = new();
+
+        public StudentController()
+        {
+            useNameList.Add("ankit");
+            useNameList.Add("Chi");
+            useNameList.Add("Hanh");
+        }
+
+        [HttpPost]
+        public IActionResult Post(StudentRegistration registrationData)
+        {
+            if (useNameList.Contains(registrationData.Username.ToLower()))
+            {
+                ModelState.AddModelError(nameof(registrationData.Username), "This user name is not availble.");
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                return Ok(ModelState);
+            }
+        }
+    }
+}
