9bcd91a80445a188c4afb6d910e4561d2d93712a
DaoHanh32 <77731242+DaoHanh32@users.noreply.github.com>
2022-01-20 10:22:29 +0700

    first commit

---
+++ Shared/Models/StrudentRegistration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoValidatorCustom.Shared.Models
+{
+    internal class StrudentRegistration
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$", ErrorMessage ="Password should have a minium 8 character, at least 1 upercase")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The Password and confirm do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+    }
+}
